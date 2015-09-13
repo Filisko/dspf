ea73fbb30697fef99317dfa4421efbc76a85abab
Filis Futsarov <filisfutsarov@gmail.com>
2015-09-13 19:02:15 +0200

    About page and assets

---
+++ application/controllers/aboutController.php
@@ -0,0 +1,20 @@
+<?php
+class aboutController extends Controller
+{
+    public function index()
+    {
+        $menu = $this->helper('Menu');
+        $menu->selected('about');
+        
+        $header = $this->view('templates/header');
+        $header->set('title', 'About');
+        $header->set('menu', $menu->getMenu());
+        $header->render();
+        
+        $template = $this->view('about/index');
+        $template->render();
+        
+        $footer = $this->view('templates/footer');
+        $footer->render();
+    }
+}
---
+++ application/helpers/menu.php
@@ -0,0 +1,40 @@
+<?php
+class Menu
+{
+    private $_menu = array();
+
+    public function __construct()
+    {
+        $this->_menu = [
+            [
+                'id' => 'home',
+                'name' => 'Home',
+                'link' => URL
+            ],
+            [
+                'id' => 'about',
+                'name' => 'About',
+                'link' => URL . 'about'
+            ]
+        ];
+    }
+
+    public function getMenu()
+    {
+        $i = 0;
+        foreach ($this->_menu as $item)
+        {
+            if (!isset($this->_menu[$i]['status'])) {
+                $this->_menu[$i]['status'] = '';
+            }
+            $i++;
+        }
+        return $this->_menu;
+    }
+    
+    public function selected($id, $class='selected')
+    {
+        $key = array_search($id, array_column($this->_menu, 'id'));
+        $this->_menu[$key]['status'] = ' ' . $class;
+    }
+}
