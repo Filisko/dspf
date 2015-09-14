a17031073bc1ba6fb5b71f99f9ebe36b4fd72537
Filis Futsarov <filisfutsarov@gmail.com>
2015-09-14 20:43:52 +0200

    Error controller and view added

---
+++ application/controllers/error.php
@@ -0,0 +1,19 @@
+<?php
+class Error extends Controller
+{
+    public function index()
+    {
+        $menu = $this->helper('Menu');
+        
+        $header = $this->view('templates/header');
+        $header->set('title', 'Error 404');
+        $header->set('menu', $menu->getMenu());
+        $header->render();
+        
+        $template = $this->view('error/index');
+        $template->render();
+        
+        $footer = $this->view('templates/footer');
+        $footer->render();
+    }
+}
